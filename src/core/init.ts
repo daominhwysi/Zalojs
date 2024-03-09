4967a0c9521afafbc5ffa6dd3e63713f0ee8fe73
DaoMinh <114815151+daominhwysi@users.noreply.github.com>
2024-03-09 16:46:11 +0700

    debug server

--- src/core/init.ts
+++ src/core/init.ts
@@ -20,7 +20,7 @@ export default async function init(options: InitOptions) {
     let isLogin = false;
     
     app.set('view engine', 'ejs');
-    app.set('views', path.join(__dirname, '../src/views'));
+    app.set('views', path.join(__dirname, '../views'));
     app.use(express.static(path.join(__dirname, 'public')));
     
 
